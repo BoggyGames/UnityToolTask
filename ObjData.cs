dc86a4d8db677271990a81e2a26be2d32eda0585
Bogdan Micić <bogdanmicic0307@gmail.com>
2025-10-16 20:56:34 +0200

    Part 1 done - unity scene hierarchy dump

---
+++ ObjData.cs
@@ -0,0 +1,9 @@
+namespace UnityToolTask
+{
+    public class ObjData
+    {
+        public long gameObj;
+        public long FileID;
+        public List<long> Children;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿//using YamlDotNet.Serialization;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityToolTask;
 
 /*
  *Important info so far:
@@ -11,8 +12,10 @@ using System.Runtime.InteropServices;
  */
 
 //strings:
-const string ebString = @"\\ProjectSettings\\EditorBuildSettings.asset";
 const string usString = @"UnusedStrings.csv";
+const string scnPattern = @"*.unity";
+const string csPattern = @"*.cs";
+const string dumpExt = @".unity.dump";
 
 if (args.Length != 2)
 {
@@ -35,27 +38,127 @@ if (!Directory.Exists(outputFolder))
 
 //part 1: scene hierarchy analysis/dumps
 
-var ebPath = projectFolder + ebString;
-
-if (!File.Exists(ebPath))
-{
-    Console.WriteLine("Are you sure this is a valid Unity project? EditorBuildSettings file does not exist: " + ebPath);
-    return 0;
-}
-
 var scenePaths = new List<string>();
+var assetPath = projectFolder + "\\Assets";
 
-var sr = new StreamReader(ebPath);
-var ebYml = sr.ReadToEnd();
-sr.Close();
-
-Console.WriteLine(ebYml);
-
-//no need to deserialize, simple foreach will do in this case
-foreach (var line in (ebYml.Split('\n')))
-    if (line.Contains("path: "))
-        scenePaths.Add(line.Trim().Split(' ')[1].Replace('/','\\'));
+//scan Assets dir for scenes
+try
+{
+    foreach (string file in Directory.EnumerateFiles(assetPath, scnPattern, SearchOption.AllDirectories))
+    {
+        scenePaths.Add(file);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Can't perform search: {ex.Message}");
+    return 1;
+}
 
 Console.WriteLine("Scenes detected:\n" + string.Join(",\n", scenePaths));
 
+//hierarchy file for each scene
+foreach (var scene in (scenePaths))
+{
+    var sr = new StreamReader(scene);
+    var sceneName = Path.GetFileNameWithoutExtension(scene);
+    var sceneContent = sr.ReadToEnd();
+    sr.Close();
+    
+    var objects = Regex.Split(sceneContent, @"(?=^--- !u!\d+ &\d+)", RegexOptions.Multiline);
+    
+    //make sure you can find them easy by searching for the fileId
+    var targets = new Dictionary<long, ObjData>();
+    var gameObjs = new Dictionary<long, string>();
+
+    var rootList = new List<ObjData>();
+    
+    //gameObjs are only used to get the gameObj name of the transform for the final hierarchy
+
+    foreach (var obj in objects)
+    {
+        var match = Regex.Match(obj, @"^--- !u!(\d+) &(\d+)", RegexOptions.Multiline);
+        if (!match.Success) continue;
+
+        int type = int.Parse(match.Groups[1].Value);
+        long fileID = long.Parse(match.Groups[2].Value);
+        
+        //4 is transform and 224 is recttransform - recttransform inherits transform so they behave the same here
+        if (type == 4 || type == 224)
+        {
+            //lines of great importance for the hierarchy!
+            var childrenLines = obj.Split("m_Children")[1].Split("m_Father")[0];
+            var gameobjLine = obj.Split('\n')
+                .FirstOrDefault(l => l.Contains("m_GameObject"));
+            var parentLine = obj.Split('\n')
+                .FirstOrDefault(l => l.Contains("m_Father"));
+            
+            //get all children
+            var children = new List<long>();
+            foreach (Match c in Regex.Matches(childrenLines, @"fileID:\s*(\d+)", RegexOptions.Multiline))
+            {
+                children.Add(long.Parse(c.Groups[1].Value));
+            }
+            
+            //make sure we know if its at root or not
+            var atRoot = (long.Parse(Regex.Match(parentLine, @"fileID:\s*(\d+)").Groups[1].Value)) == 0;
+            
+            //get corresponding gameobj for name
+            var gameObj = long.Parse(Regex.Match(gameobjLine, @"fileID:\s*(\d+)").Groups[1].Value);
+
+            var newObj = new ObjData()
+            {
+                gameObj = gameObj,
+                FileID = fileID,
+                Children = children
+            };
+            
+            targets.Add(fileID, newObj);
+            
+            //if at root, use to start recursion later
+            if (atRoot)
+                rootList.Add(newObj);
+        }
+        else if (type == 1)
+        {
+            //1 is gameobject - get the name for the dictionary
+            var nameLine = obj.Split('\n')
+                .FirstOrDefault(l => l.Contains("m_Name"));
+            
+            //no regex needed here, just remove the first part
+            var name = nameLine.Trim().Substring(8);
+            
+            gameObjs.Add(fileID, name);
+        }
+    }
+    
+    //finally, make the tree for this scene - recurse over these dicts from root
+    var tree = "";
+    foreach (var obj in rootList)
+        tree += BuildTree(obj, 0, targets, gameObjs);
+
+    //Console.WriteLine(tree);
+
+    var sw = new StreamWriter(outputFolder + "\\" + sceneName + dumpExt);
+    sw.Write(tree);
+    sw.Close();
+}
+
 return 0;
+
+//helper recursive function for writing the tree string
+string BuildTree(ObjData current, int depth, Dictionary<long, ObjData> setObj, Dictionary<long, string> names)
+{
+    var childrenSet = "";
+    var childrenDepth = "";
+    for (int i = 0; i <= depth; i++)
+        childrenDepth += "--";
+
+    foreach (var child in current.Children)
+    {
+        childrenSet += childrenDepth + BuildTree(setObj[child], depth + 1, setObj, names);
+    }
+        
+    return names[current.gameObj] + '\n' + childrenSet;
+}
+
